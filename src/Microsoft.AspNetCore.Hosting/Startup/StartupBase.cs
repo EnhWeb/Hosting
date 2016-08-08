7556d29381df67a69ee84b688a6a05b2e8825518
Kristian Hellang <kristian@hellang.com>
2016-08-08 16:33:16 +0200

    Added new StartupBase

--- src/Microsoft.AspNetCore.Hosting/Startup/StartupBase.cs
+++ src/Microsoft.AspNetCore.Hosting/Startup/StartupBase.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,6 +15,52 @@ namespace Microsoft.AspNetCore.Hosting
         public virtual IServiceProvider ConfigureServices(IServiceCollection services)
         {
             return services.BuildServiceProvider();
+        }
+    }
+
+    public abstract class StartupBase<TBuilder> : IStartup
+    {
+        protected StartupBase(IServiceProviderFactory factory)
+        {
+            if (factory == null)
+            {
+                throw new ArgumentNullException(nameof(factory));
+            }
+
+            if (!IsCompatible(factory))
+            {
+                throw new ArgumentException("Startup class is not compatible with the current IServiceProviderFactory.", nameof(factory));
+            }
+
+            Factory = factory;
+        }
+
+        private IServiceProviderFactory Factory { get; }
+
+        IServiceProvider IStartup.ConfigureServices(IServiceCollection services)
+        {
+            ConfigureServices(services);
+
+            var builder = Factory.CreateContainerBuilder(services);
+
+            ConfigureContainer((TBuilder) builder);
+
+            return Factory.CreateServiceProvider(builder);
+        }
+
+        protected virtual void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        protected virtual void ConfigureContainer(TBuilder builder)
+        {
+        }
+
+        public abstract void Configure(IApplicationBuilder app);
+
+        private static bool IsCompatible(IServiceProviderFactory factory)
+        {
+            return typeof(TBuilder).GetTypeInfo().IsAssignableFrom(factory.ContainerBuilderType.GetTypeInfo());
         }
     }
 }
